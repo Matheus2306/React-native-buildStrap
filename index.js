6023f31dd6e34df0ab6cbbd67bc78dfea657e68d
matheus2306 <math2306.r@gmail.com>
2025-10-07 10:45:55 -0300

    Update version to 1.0.4 in package.json; import useMemo in index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useState } from "react";
+import React, { createContext, useContext, useMemo, useState } from "react";
 import { DarkTheme, LightTheme } from "./Themes/Themes";
 
 const ThemeContext = createContext();
